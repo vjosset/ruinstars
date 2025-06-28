909b31dca58f2ea0c4b679fc3f8744a87e32055e
vjosset <lkjasd>
2025-06-28 00:46:36 -0400

    Cleanup, everything is fixed now

--- src/components/shared/Links.tsx
+++ src/components/shared/Links.tsx
@@ -1,11 +1,11 @@
 'use client'
 
-import Link from 'next/link';
-import { FiList, FiUser, FiUsers } from 'react-icons/fi';
+import Link from 'next/link'
+import { FiList, FiUser, FiUsers } from 'react-icons/fi'
 
 const badgeClass = 'text-muted inline-flex items-center gap-1 px-1 py-0.5 text-sm font-medium rounded border border-main hover:bg-main/10'
 
-export function FactionLink({ factionId, factionName }: { factionId: string; factionName: string }) {
+export function FactionLink({ factionId, factionName }: { factionId: string, factionName: string }) {
   return (
     <Link href={`/factions/${factionId}`} className={badgeClass}>
       <FiList />
@@ -23,7 +23,7 @@ export function UserLink({ userName }: {userName: string}) {
   )
 }
 
-export function SquadLink({ squadId, squadName }: { squadId: string; squadName: string }) {
+export function SquadLink({ squadId, squadName }: { squadId: string, squadName: string }) {
   return (
     <Link href={`/squads/${squadId}`} className={badgeClass}>
       <FiUsers />
