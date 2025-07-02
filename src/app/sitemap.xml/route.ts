4834dc3cf806f4ac383a2b429cdd4effb96b4ca6
vjosset <lkjasd>
2025-07-02 10:45:01 -0400

    Cleanup

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -46,8 +46,6 @@ export async function GET() {
   // Build full list of URLs
   const urls = [...staticUrls, ...dynamicUrls]
 
-  console.log('Total URLs:', urls.length)
-
   const body = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${urls
