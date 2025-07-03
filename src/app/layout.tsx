4d136ed9e990392cbbfb04dde5f294a3e34af084
vjosset <lkjasd>
2025-07-03 15:46:54 -0400

    Messing with fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,9 @@ export default async function RootLayout({ children }: { children: React.ReactNo
             });
           `}
         </Script>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""></link>
+        <link href="https://fonts.googleapis.com/css2?family=Chakra+Petch:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Goldman:wght@400;700&family=Keania+One&family=Oxanium:wght@200..800&family=Quantico:ital,wght@0,400;0,700;1,400;1,700&family=Russo+One&family=Tektur:wght@400..900&family=Tomorrow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
       </head>
   
       <body className="text-foreground font-main">
