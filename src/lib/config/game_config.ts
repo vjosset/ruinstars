5050faef995e70b0905996f68f5089ac8794810c
vjosset <lkjasd>
2025-08-26 05:24:46 -0400

    Revert to D6 - D10 was less fun

--- src/lib/config/game_config.ts
+++ src/lib/config/game_config.ts
@@ -1,5 +1,5 @@
 export const GAME = {
   NAME: 'Ruinstars',
-  DICE_BASIS: 10,
-  ROOT_URL: 'https://dev.ruinstars.com' //CHANGE THIS BEFORE PROD
+  DICE_BASIS: 6,
+  ROOT_URL: 'https://ruinstars.com' //CHANGE THIS BEFORE PROD
 } as const
