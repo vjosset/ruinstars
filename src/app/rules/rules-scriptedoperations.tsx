45507ef85b2731b0ea9226b59c9f919f27f56d6a
vjosset <lkjasd>
2025-09-22 14:28:21 -0400

    Tweak rulebook (print only)

--- src/app/rules/rules-scriptedoperations.tsx
+++ src/app/rules/rules-scriptedoperations.tsx
@@ -16,6 +16,9 @@ export default async function RulesScriptedOperations() {
         Instead of playing a primary mission or rolling your own Campaign, you may choose to play a Scripted Operation.<br/>
         Scripted Operations are pre-built mini-campaigns tailored to specific squads and factions, and include branching missions based on successes and failures.
       </p>
+      <p className="printonly">
+        Refer to the end of this rule book for the complete list of Scripted Operations and their Missions.
+      </p>
       <ScriptedOperationsList operations={operations} squadTypes={squadTypes} />
     </div>
   )}
