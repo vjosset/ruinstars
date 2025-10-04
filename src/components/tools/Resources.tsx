058775f87037eef6ccc4b00d5a4d5e515dc81d4f
vjosset <lkjasd>
2025-10-04 10:39:14 -0400

    Update resources

--- src/components/tools/Resources.tsx
+++ src/components/tools/Resources.tsx
@@ -9,50 +9,55 @@ export default function Resources() {
         <p className="text-muted">
           Print-at-home resources for Ruinstars, including PDFs for the rules and battlefields, and STLs for 3D printing tiles and terrain.
         </p>
-        <ul>
-          <li>
-            Rules:
-            <ul>
-              <li>
-                <Link className="underline" target="_blank" href="/assets/Ruinstars - The Rules - 20251004.pdf">Easy Print</Link>
-                { ' / ' }
-                <Link className="underline" target="_blank" href="/assets/Ruinstars - The Rules - 20251004 - FullColor.pdf">Full Color</Link>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <Link className="underline" target="_blank" href="/assets/Ruinstars_Tokens.pdf">Tokens</Link>
-          </li>
-          <li>
-            <Link className="underline" target="_blank" href="/assets/Ruinstars_Gauge_Letter.pdf">Gauge (US Letter)</Link>
-          </li>
-          <li>
-            <Link className="underline" target="_blank" href="/assets/Ruinstars_Gauge_A4.pdf">Gauge (A4)</Link>
-          </li>
-          <li>
-            Print-at-home Battlefield PDFs:
-            <ul>
-              <li>The Ruined City: { ' ' }
-                <Link className="underline" target="_blank" href="/assets/battlefields\TheRuinedCity_Letter.pdf">US Letter</Link>
-                { ' / ' }
-                <Link className="underline" target="_blank" href="/assets/battlefields\TheRuinedCity_A4.pdf">A4</Link>
-              </li>
-              <li>The Facility: { ' ' }
-                <Link className="underline" target="_blank" href="/assets/battlefields\TheFacility_Letter.pdf">US Letter</Link>
-                { ' / ' }
-                <Link className="underline" target="_blank" href="/assets/battlefields\TheFacility_A4.pdf">A4</Link>
-              </li>
-              <li>No Man's Land:  { ' ' }
-                <Link className="underline" target="_blank" href="/assets/battlefields\NoMansLand_Letter.pdf">US Letter</Link>
-                { ' / ' }
-                <Link className="underline" target="_blank" href="/assets/battlefields\NoMansLand_A4.pdf">A4</Link>
-              </li>
-            </ul>
-            These PDFs are composed of 9 tiles of 20cm x 20cm each with grid lines marking the Squares.
-            Print them at home at 100% scale, cut them out, and glue them to cardboard, foamboard, or wood for a sturdy battlefield.
-          </li>
-          <li>STLs for 3D Printing: <em>Coming soon!</em></li>
-        </ul>
+        <div>
+          <h6>Rules</h6>
+          <ul>
+            <li><Link className="underline" target="_blank" href="/assets/Ruinstars - The Rules - 20251004.pdf">Easy Print</Link></li>
+            <li><Link className="underline" target="_blank" href="/assets/Ruinstars - The Rules - 20251004 - FullColor.pdf">Full Color</Link></li>
+          </ul>
+        </div>
+        <div>
+          <h6>Tokens</h6>
+          <ul>
+            <li><Link className="underline" target="_blank" href="/assets/Ruinstars_Tokens.pdf">Tokens</Link></li>
+          </ul>
+        </div>
+        <div>
+          <h6>Gauges</h6>
+          <ul>
+            <li>
+              <Link className="underline" target="_blank" href="/assets/Ruinstars_Gauge_Letter.pdf">Gauge (US Letter)</Link>
+            </li>
+            <li>
+              <Link className="underline" target="_blank" href="/assets/Ruinstars_Gauge_A4.pdf">Gauge (A4)</Link>
+            </li>
+            <li>
+              <Link className="underline" target="_blank" href="https://www.thingiverse.com/thing:7162556">STL</Link> (for 3d printing)
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h6>Battlefields</h6>
+          <ul>
+            <li>The Ruined City: { ' ' }
+              <Link className="underline" target="_blank" href="/assets/battlefields\TheRuinedCity_Letter.pdf">US Letter</Link>
+              { ' / ' }
+              <Link className="underline" target="_blank" href="/assets/battlefields\TheRuinedCity_A4.pdf">A4</Link>
+            </li>
+            <li>The Facility: { ' ' }
+              <Link className="underline" target="_blank" href="/assets/battlefields\TheFacility_Letter.pdf">US Letter</Link>
+              { ' / ' }
+              <Link className="underline" target="_blank" href="/assets/battlefields\TheFacility_A4.pdf">A4</Link>
+            </li>
+            <li>No Man's Land:  { ' ' }
+              <Link className="underline" target="_blank" href="/assets/battlefields\NoMansLand_Letter.pdf">US Letter</Link>
+              { ' / ' }
+              <Link className="underline" target="_blank" href="/assets/battlefields\NoMansLand_A4.pdf">A4</Link>
+            </li>
+          </ul>
+          These PDFs are composed of 9 tiles of 20cm x 20cm each with grid lines marking the Squares.
+          Print them at home at 100% scale, cut them out, and glue them to cardboard, foamboard, or wood for a sturdy battlefield.
+        </div>
       </div>
       <div className="section">
         <SectionTitle>Community</SectionTitle>
@@ -62,7 +67,7 @@ export default function Resources() {
         </p>
         <ul>
           <li><Link href="https://discord.gg/Rh8vJzkCrT" target="_blank" className="underline">Discord</Link></li>
-          <li><Link href="https://ruinstars.itch.io/ruinstars" target="_blank" className="underline">itch.io</Link></li>
+          <li><Link href="https://ruinstars.itch.io/ruinstars" target="_blank" className="underline">itch.io</Link> - Includes devlogs for the game</li>
           <li><Link href="https://github.com/vjosset/ruinstars" target="_blank" className="underline">GitHub</Link></li>
         </ul>
       </div>
