d886b6801ac2c6aa124c5708aa74f38b0aaa88bb
vjosset <lkjasd>
2025-10-06 07:53:02 -0400

    Fixes for rulebook PDF

--- src/app/rules/page.tsx
+++ src/app/rules/page.tsx
@@ -40,11 +40,22 @@ export async function generateMetadata() {
 export default async function Home() {
   const operations = ops
   const squadTypes = await SquadTypeService.getAllSquadTypes()
+  const versionTimestamp = new Intl.DateTimeFormat('en-CA', {
+    timeZone: 'UTC',
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit'
+  }).format(new Date()).replaceAll('-', '')
   
   return (
     <>
       {/* Cover */}
       <img src="/img/rules/BookCover_Framed.webp" className="printonly fullpage overflow-y-hidden" style={{pageBreakAfter: 'always'}} />
+      <div className="printonly absolute left-1/2 top-1/4 -translate-x-1/2">
+        <div className="text-white font-title text-2xl tracking-wide bg-black/70 px-6 py-3 rounded-lg shadow-lg">
+          Version {versionTimestamp}
+        </div>
+      </div>
 
       <div className="rules px-3 max-w-7xl mx-auto">
         <RulesHeader />
