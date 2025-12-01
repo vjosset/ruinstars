965542b0f788f2e0c063e8221bff12ce28d809c3
vjosset <lkjasd>
2025-12-01 08:09:25 -0500

    Clarify homebase sequence

--- src/app/rules/rules-campaigns.tsx
+++ src/app/rules/rules-campaigns.tsx
@@ -28,19 +28,12 @@ export default async function RulesCampaigns({ num }: {num?: Number | null}) {
         </div>
         <div className="section">
           <h4>Homebase</h4>
-          At the end of each Operation (after the third Mission), your Squad returns to Homebase to heal injuries, resupply, and utilize accrued resources.<br/>
-          The sequence is as follows:
+          At the end of each Operation (after the third Mission), your Squad returns to Homebase to heal injuries, resupply, and utilize accrued resources.
           <ol>
-            <li>Calculate GP and XP: All GP and XP earned during the Operation are now available for spending.</li>
-            <li>
-              Unit Status Review: 
-              <ul>
-                <li>Remove Deceased Units: Remove all Units designated as Deceased from your Squad</li>
-                <li>Heal Injuries: Remove all Injuries (except Deceased) from all remaining Standing Units</li>
-              </ul>
-            </li>
-            <li>Recruit & Resupply: Recruit new Units into the Squad. Deceased Units can only be replaced during this Homebase phase.</li>
-            <li>Squad Changes: Make changes to your Squad's selected Gear and Spoils Of War</li>
+            <li>Remove all Deceased Units from your Squad</li>
+            <li>Remove all Injuries from remaining Units</li>
+            <li>Recruit new Units into the Squad. Deceased Units can only be replaced during this Homebase phase.</li>
+            <li>Make changes to your Squad's selected Gear and Spoils Of War</li>
           </ol>
         </div>
         <div className="section">
