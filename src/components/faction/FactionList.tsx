de422e9df56c49d57a666d7fb5988e1eec4752c3
vjosset <lkjasd>
2026-02-02 07:31:17 -0500

    Update faction list to hide factions that have no squadtypes (e.g. Spec/NPC)

--- src/components/faction/FactionList.tsx
+++ src/components/faction/FactionList.tsx
@@ -12,6 +12,7 @@ export default async function FactionList({
 }: FactionListProps) {
   if (!factions || factions.length == 0) {
     factions = await FactionService.getAllFactions()
+    factions = factions.filter((fa) => fa.squadTypes.length > 0)
   }
   return (
     <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
