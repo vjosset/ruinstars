462fa933092afa92d5bbcd85ebee833353e0b1ad
vjosset <lkjasd>
2026-02-26 07:23:14 -0500

    Tweak

--- src/app/rules/corerules/sections/rules-intro.tsx
+++ src/app/rules/corerules/sections/rules-intro.tsx
@@ -60,7 +60,7 @@ export default async function RulesIntro({ num, showTitle }: {num?: number | nul
           <div className="section">
             <h3>The App</h3>
             Draft a roster, manage your GP budget, and share it with a link.<br/>
-            <Link className="section underline" href="/auth/signup">Create a free account</Link> to get started
+            <Link className="section underline" href="/auth/signup">Create a free account</Link> to get started.
           </div>
         </div>
       </div>
